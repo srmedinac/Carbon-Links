563b5e65f03c957dc235f432a3f6e789f8b1e1be
viversba <nviverosb@unal.edu.co>
2019-07-09 09:38:48 -0500

    Tetrahedron made

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public float radius = 1;
+
+    void OnDrawGizmosSelected() {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
